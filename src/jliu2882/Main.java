1431bd5c2fbdcc48b61eb6b52aee07741b8e9698
BT_1N3_32 <jliu2882>
2018-12-17 11:41:43 -0500

    added some comments

--- src/jliu2882/Main.java
+++ src/jliu2882/Main.java
@@ -3,6 +3,9 @@ package jliu2882;
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * Creates the WordList animals, and adds some animals
+         */
         WordList animals = new WordList();
         animals.addWords("cat");
         animals.addWords("mouse");
@@ -10,10 +13,17 @@ public class Main {
         animals.addWords("dog");
         animals.addWords("dog");
 
+        /**
+         * Prints the number of animals whose String is the same length as 4, 3, and 2
+         */
         System.out.println(animals.numWordsOfLength(4));
         System.out.println(animals.numWordsOfLength(3));
         System.out.println(animals.numWordsOfLength(2));
 
+        /**
+         * Removes the words of length 4, 3, and 2
+         * Print it after each time
+         */
         animals.removeWordzOfLength(4);
         System.out.println(animals.toString());
         animals.removeWordzOfLength(3);
--- src/jliu2882/WordList.java
+++ src/jliu2882/WordList.java
@@ -3,15 +3,32 @@ package jliu2882;
 import java.util.ArrayList;
 
 public class WordList {
+    /**
+     * Creates an list of Strings
+     * Originally did not have <String>
+     */
     private ArrayList<String> myList;
 
+    /**
+     * Initializes the parameter set up
+     */
     public WordList(){
         this.myList = new ArrayList<>();
     }
 
+    /**
+     * Created a function that adds words to the list
+     * @param word is the String added to the list
+     */
     public void addWords(String word){
         myList.add(word);
     }
+
+    /**
+     * Created a function that loops through the function for words of same length
+     * @param len is the length of words we are looking for
+     * @return the number of words of len length
+     */
     public int numWordsOfLength(int len){
         int count = 0;
         for(String words : myList){
@@ -21,6 +38,11 @@ public class WordList {
         }
         return count;
     }
+
+    /**
+     * Created a function that loops through the function and removes words of len length
+     * @param len is the length of words we are removing
+     */
     public void removeWordzOfLength(int len){
         for(int i = myList.size()-1; i >= 0;i--){
             if(myList.get(i).length()==len){
@@ -28,6 +50,11 @@ public class WordList {
             }
         }
     }
+
+    /**
+     * Created a function to print the list
+     * @return the list in String format
+     */
     public String toString(){
         String result = "";
         for(String words : myList){
